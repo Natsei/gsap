b2c5e743ee16696bc5910723772f86fccce9ae71
Natsei <83393771+Natsei@users.noreply.github.com>
2025-05-11 16:25:54 +0200

    Updated next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,22 @@
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  webpack: (config, { dev, isServer }) => {
+    if (dev) {
+      config.cache = {
+        type: 'filesystem',
+        cacheDirectory: path.resolve(__dirname, '.next', 'cache', 'webpack'),
+        buildDependencies: {
+          config: [__filename]
+        }
+      };
+    }
+    return config;
+  }
+};
 
 export default nextConfig;
